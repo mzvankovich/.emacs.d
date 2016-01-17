f1196d2e82c4c61258b5a7018033b65bd01d3d51
Dan Midwood <dan@danmidwood.com>
2016-01-17 01:38:09 -0300

    Remove no-longer-required magit fit

--- crumpet/magit.el
+++ crumpet/magit.el
@@ -1,10 +1,6 @@
 ;;; Add the bin folder containing the git binary to the exec path
 (add-to-list 'exec-path "/usr/local/bin/")
 
-;;; Magit now breaks trying to open a new emacs for commit messages
-;;; This fixes it
-;;; Fix from: http://stackoverflow.com/questions/18856047/emacs-magit-commit-opens-new-emacs-client#comment28590245_19265280
-(set-variable 'magit-emacsclient-executable "/usr/local/Cellar/emacs/24.5/bin/emacsclient")
 ;;; Stop nagging about the upstream branch
 (setq magit-push-always-verify nil)
 ;; This seems to be a popular choice, Emacs Prelude uses it

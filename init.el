465e99ecbe12026e8921ac32de88ead866515a36
Dan Midwood <dan@danmidwood.com>
2017-02-26 12:19:17 -0300

    Add package initialize call

--- init.el
+++ init.el
@@ -1,3 +1,10 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (let* ((conf-dir (file-name-directory (or load-file-name "~/.emacs.d/")))
       (preload-dir (expand-file-name "preload" conf-dir))
       (custom-dir (expand-file-name "crumpet" conf-dir)))
@@ -18,3 +25,17 @@
      clojure-mode
      cider))
   (mapc 'load (directory-files custom-dir 't "^[^#].*el$")))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (cider clojure-mode rainbow-delimiters paredit expand-region magit company solarized-theme helm))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
